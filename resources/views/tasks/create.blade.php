165c672348139df3ab6ad4abf69ae0b6c5f8cfac
kota.yamada <ec2-user@ip-172-31-5-57.us-east-2.compute.internal>
2018-05-28 05:38:05 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,8 +6,13 @@
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
+        
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
+        
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+        
 
         {!! Form::submit('作成') !!}
 
